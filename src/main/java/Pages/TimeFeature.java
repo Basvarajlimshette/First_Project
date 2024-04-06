3e1a2b7358b2d078f07532117f283f7762d15803
basvarajlimshette <basvarajlimshette@gmail.com>
2024-04-06 18:17:27 +0530

    TimeFeature page and HomePageTest Added

--- src/main/java/Pages/TimeFeature.java
+++ src/main/java/Pages/TimeFeature.java
@@ -1,5 +1,28 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TimeFeature {
+	
+	
+	
+	WebDriver driver;
+	By Clock =By.xpath("");
+	
+	
+	public TimeFeature() {
+		
+		driver=new ChromeDriver();
+		
+	}
+	
+
+	public void correctTime() {
+		
+		System.out.println("Time ="+ "10:00 Am");
+	}
+	
 
 }
--- src/test/java/testclases/HomepageTest.java
+++ src/test/java/testclases/HomepageTest.java
@@ -1,5 +1,12 @@
 package testclases;
 
 public class HomepageTest {
+	
+	
+	public void Time() {
+		System.out.println("Your Boss");
+
+	}
+	
 
 }
